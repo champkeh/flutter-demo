1e6794e06df396d7aa63761e33b4944bf4e6878a
champkeh <champion.ge@gmail.com>
2019-06-13 16:21:44 +0800

    更新代码

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_demo/animation/stagger_animation.dart';
+import 'package:flutter/rendering.dart';
 
 void main() => runApp(MyApp());
 
@@ -16,12 +17,58 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Demo'),
         ),
-        body: StaggerRoute(),
+        body: ConstrainedBox(
+          constraints: BoxConstraints(
+            maxWidth: double.infinity,
+            minWidth: 100,
+            maxHeight: 200,
+            minHeight: 100,
+          ),
+          child: Stingy(
+            child: RaisedButton(
+              child: Text('Hello there...'),
+              onPressed: () {
+                print('It\'s been so long');
+              },
+              color: Colors.red,
+            ),
+          ),
+        ),
       ),
     );
   }
 }
 
+class Stingy extends SingleChildRenderObjectWidget {
+  Stingy({Widget child}) : super(child: child);
+
+  @override
+  RenderObject createRenderObject(BuildContext context) {
+    return RenderStingy();
+  }
+}
+
+class RenderStingy extends RenderShiftedBox {
+  RenderStingy() : super(null);
+
+  @override
+  void performLayout() {
+    child.layout(
+        BoxConstraints(
+          minHeight: 0,
+          maxHeight: constraints.minHeight,
+          minWidth: 0,
+          maxWidth: constraints.minWidth,
+        ),
+        parentUsesSize: true);
+
+    final BoxParentData childParentData = child.parentData;
+    childParentData.offset = Offset(
+        this.constraints.maxWidth - child.size.width,
+        this.constraints.maxHeight - child.size.height);
+    size = Size(this.constraints.maxWidth, constraints.maxHeight);
+  }
+}
 
 class HomePage extends StatefulWidget {
   @override
